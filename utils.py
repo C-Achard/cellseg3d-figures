d08606ebf6e774f13b6a3d2bc2adcfc3b9ce42db
C-Achard <cyril.achard@epfl.ch>
2024-02-26 10:05:06 +0100

    Add global parameters for the figures

---
+++ utils.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+"""
+Utilities for the figures : colormap, path to data, etc.
+"""
+
+
+
+GLOBAL_COLORMAP = "mako"
+GLOBAL_DPI = 300
+GLOBAL_DATA_PATH = Path.home() / "Desktop/Code/CELLSEG_BENCHMARK/"
