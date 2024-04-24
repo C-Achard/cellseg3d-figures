2e4a764a0a02160f320e5a17d15803ba21c112bd
C-Achard <cyril.achard@epfl.ch>
2024-04-24 10:44:38 +0200

    Fix path

--- figures/fig1-f-g-splits/training/cellseg3d_training.py
+++ figures/fig1-f-g-splits/training/cellseg3d_training.py
@@ -16,10 +16,11 @@ WANDB_MODE = "disabled"
 
 sys.path.append("../..")
 
-DATA_PATH = (
-    Path.home()
-    / "Desktop/Code/CELLSEG_BENCHMARK/TPH2_mesospim/TRAINING/SPLITS"
-)
+# DATA_PATH = (
+#     Path.home()
+#     / "Desktop/Code/CELLSEG_BENCHMARK/TPH2_mesospim/TRAINING/SPLITS"
+# )
+DATA_PATH = Path("/data/cyril/CELLSEG_BENCHMARK/TPH2_mesospim/TRAINING/SPLITS")
 
 TRAINING_PERCENTAGES = [10, 20, 40, 80]
 MODELS = ["SegResNet", "SwinUNetR"]
