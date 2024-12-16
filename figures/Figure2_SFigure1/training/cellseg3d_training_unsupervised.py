af58dbc8639af4274a8016af124e65d08ded4e5f
MMathisLab <mathis@rowland.harvard.edu>
2024-12-16 20:59:18 +0100

    renaming

---
+++ figures/Figure2_SFigure1/training/cellseg3d_training_unsupervised.py
@@ -0,0 +1,173 @@
+"""Showcases how to train a model without napari."""
+
+import sys
+from pathlib import Path
+
+import torch
+from napari_cellseg3d import config as cfg
+from napari_cellseg3d.code_models.worker_training import (
+    SupervisedTrainingWorker,
+    WNetTrainingWorker,
+)
+from napari_cellseg3d.utils import LOGGER as logger
+from napari_cellseg3d.utils import get_all_matching_files
+
+# set wandb mode globally
+WANDB_MODE = "online"  # "disabled"
+DEVICE = "cuda:3"
+
+sys.path.append("../..")
+
+# DATA_PATH = (
+#     Path.home()
+#     / "Desktop/Code/CELLSEG_BENCHMARK/TPH2_mesospim/TRAINING/SPLITS"
+# )
+DATA_PATH = Path(
+    "/data/cyril/CELLSEG_BENCHMARK/TPH2_mesospim/TRAINING/SPLITS/file_percentage"
+)
+
+TRAINING_PERCENTAGES = [
+    # 10,
+    # 20,
+    # 40,
+    60,
+    # 80
+]
+SEEDS = [34936339, 34936397, 34936345]
+
+
+def train_wnet_on_splits(training_splits, seeds, skip_existing=False):
+    """Trains WNet on different training splits."""
+    for seed in seeds:
+        for training_split in training_splits:
+            print(f"Training WNet on {training_split}% with seed {seed}")
+            remote_training_unsupervised(training_split, seed, skip_existing)
+
+
+class LogFixture:
+    """Fixture for napari-less logging, replaces napari_cellseg3d.interface.Log in model_workers.
+
+    This allows to redirect the output of the workers to stdout instead of a specialized widget.
+    """
+
+    def __init__(self):
+        """Creates a LogFixture object."""
+        super(LogFixture, self).__init__()
+
+    def print_and_log(self, text, printing=None):
+        """Prints and logs text."""
+        print(text)
+
+    def warn(self, warning):
+        """Logs warning."""
+        logger.warning(warning)
+
+    def error(self, e):
+        """Logs error."""
+        raise (e)
+
+
+def prepare_data(images_path, results_path):
+    """Prepares data for training."""
+    assert images_path.exists(), f"Images path does not exist: {images_path}"
+    if not results_path.exists():
+        results_path.mkdir(parents=True)
+
+    images = get_all_matching_files(images_path)
+
+    print(f"Images paths: {images}")
+
+    logger.info("Images :\n")
+    for file in images:
+        logger.info(Path(file).name)
+    logger.info("*" * 10)
+
+    return [{"image": str(image_path)} for image_path in images]
+
+
+def remote_training_unsupervised(training_split, seed, skip_existing=False):
+    """Function to train a model without napari."""
+    # print(f"Results path: {RESULTS_PATH.resolve()}")
+    batch_size = 4
+    results_path = (
+        Path("/data/cyril")
+        / "CELLSEG_BENCHMARK/cellseg3d_train"
+        / f"WNet_{training_split}_{seed}"
+    )
+    if skip_existing and results_path.exists():
+        # ask user if they want to continue training
+        print(
+            f"Results path {results_path} already exists. Continue training? (y/n)"
+        )
+        answer = input()
+        if answer.lower() != "y":
+            print("Training aborted.")
+            return
+
+    data_path = DATA_PATH / str(training_split)
+    data_dict = prepare_data(data_path, results_path)
+
+    data_dict = [{"image": d["image"]} for d in data_dict]
+
+    wandb_config = cfg.WandBConfig(
+        mode="online",
+        save_model_artifact=True,
+    )
+
+    deterministic_config = cfg.DeterministicConfig(
+        seed=seed,
+    )
+
+    worker_config = cfg.WNetTrainingWorkerConfig(
+        device=DEVICE,
+        max_epochs=int(6000 / training_split),  # 50,
+        # model params
+        in_channels=1,
+        out_channels=1,
+        num_classes=2,
+        dropout=0.65,
+        learning_rate=2e-5,
+        use_clipping=False,
+        clipping=1.0,
+        weight_decay=0.01,  # 1e-5
+        # NCuts loss params
+        intensity_sigma=1.0,
+        spatial_sigma=4.0,
+        radius=2,
+        # reconstruction loss params
+        reconstruction_loss="MSE",  # or "BCE"
+        # summed losses weights
+        n_cuts_weight=0.5,
+        rec_loss_weight=0.5 / 1.0,  # 0.5 / 100
+        validation_interval=2,
+        batch_size=batch_size,
+        deterministic_config=deterministic_config,
+        scheduler_factor=0.5,
+        scheduler_patience=10,  # use default scheduler
+        weights_info=cfg.WeightsInfo(),  # no pretrained weights
+        results_path_folder=str(results_path),
+        sampling=False,
+        do_augmentation=True,
+        train_data_dict=data_dict,
+        # eval_data_dict=val_data_dict,
+    )
+
+    worker = WNetTrainingWorker(worker_config)
+    worker.wandb_config = wandb_config
+    ######### SET LOG
+    log = LogFixture()
+    worker.log_signal.connect(log.print_and_log)
+    worker.warn_signal.connect(log.warn)
+    worker.error_signal.connect(log.error)
+
+    results = []
+    for result in worker.train(wandb_name_override=results_path.name):
+        results.append(result)
+    print("Training finished")
+    del results
+    del worker
+    torch.cuda.empty_cache()
+
+
+if __name__ == "__main__":
+    train_wnet_on_splits(TRAINING_PERCENTAGES, SEEDS, skip_existing=True)
---
+++ figures/Figure2_SFigure1/training/stardist_train_on_splits.py
@@ -0,0 +1,227 @@
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
+
+import pathlib as pt
+import shutil
+import sys
+from glob import glob
+from os import environ
+
+import numpy as np
+import tensorflow as tf
+from csbdeep.utils import Path, normalize
+from stardist import (
+    Rays_GoldenSpiral,
+    calculate_extents,
+    fill_label_holes,
+    gputools_available,
+    random_label_cmap,
+)
+from stardist.matching import matching, matching_dataset
+from stardist.models import Config3D, StarDist3D, StarDistData3D
+from tifffile import imread
+from tqdm import tqdm
+
+DATA_PATH = Path.home() / "Desktop/Code/CELLSEG_BENCHMARK/TPH2_mesospim/SPLITS"
+
+NUM_EPOCHS = 50
+TRAINING_PERCENTAGES = [10, 20, 60, 80]
+SPLITS = ["1_c15", "2_c1_c4_visual", "3_c1245_visual"]
+SEED = 34936339
+
+
+def train_stardist(path_images, train_percentage, seed=SEED):
+    # Set seeds for numpy & tensorflow
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
+    environ["PYTHONHASHSEED"] = "0"
+    environ["TF_DETERMINISTIC_OPS"] = "1"
+    # tf.config.threading.set_inter_op_parallelism_threads(1)
+    # tf.config.threading.set_intra_op_parallelism_threads(1)
+
+    X_paths = sorted(glob(str(path_images / "*.tif")))
+    Y_paths = sorted(glob(str(path_images / "labels/*.tif")))
+    # assert all(Path(x).name==Path(y).name for x,y in zip(X,Y))
+    X = list(map(imread, X_paths))
+    Y = list(map(imread, Y_paths))
+    # Y = [Yi.astype(np.uint16) for Yi in Y]
+    n_channel = 1 if X[0].ndim == 3 else X[0].shape[-1]
+    axis_norm = (0, 1, 2)  # normalize channels independently
+    # axis_norm = (0,1,2,3) # normalize channels jointly
+    if n_channel > 1:
+        print(
+            "Normalizing image channels %s."
+            % (
+                "jointly"
+                if axis_norm is None or 3 in axis_norm
+                else "independently"
+            )
+        )
+        sys.stdout.flush()
+
+    X = [normalize(x, 1, 99.8, axis=axis_norm) for x in tqdm(X)]
+    Y = [fill_label_holes(y) for y in tqdm(Y)]
+    rng = np.random.RandomState(seed)
+    ind = rng.permutation(len(X))
+    print(ind)
+    ind = range(len(X))
+    print(ind)
+    val_percent = 1 - train_percentage / 100
+    n_val = max(1, int(round(val_percent * len(ind))))
+    ind_train, ind_val = ind[:-n_val], ind[-n_val:]
+    X_val, Y_val = [X[i] for i in ind_val], [Y[i] for i in ind_val]
+    X_trn, Y_trn = [X[i] for i in ind_train], [Y[i] for i in ind_train]
+    print("number of images: %3d" % len(X))
+    print("- training:       %3d" % len(X_trn))
+    print("- validation:     %3d" % len(X_val))
+    print("Train files :")
+    [print(X_paths[i]) for i in ind_train]
+    print("Validation files :")
+    [print(X_paths[i]) for i in ind_val]
+    print("*" * 20)
+
+    print(Config3D.__doc__)
+    extents = calculate_extents(Y)
+    anisotropy = tuple(np.max(extents) / extents)
+    print("empirical anisotropy of labeled objects = %s" % str(anisotropy))
+    # 96 is a good default choice (see 1_data.ipynb)
+    n_rays = 96
+
+    # Use OpenCL-based computations for data generator during training (requires 'gputools')
+    # use_gpu = False and gputools_available()
+    use_gpu = True
+
+    # Predict on subsampled grid for increased efficiency and larger field of view
+    # grid = tuple(1 if a > 1.5 else 2 for a in anisotropy)
+    grid = (1, 1, 1)
+
+    # Use rays on a Fibonacci lattice adjusted for measured anisotropy of the training data
+    rays = Rays_GoldenSpiral(n_rays, anisotropy=anisotropy)
+
+    conf = Config3D(
+        rays=rays,
+        grid=grid,
+        anisotropy=anisotropy,
+        use_gpu=use_gpu,
+        n_channel_in=n_channel,
+        # adjust for your data below (make patch size as large as possible)
+        train_patch_size=(64, 64, 64),
+        # train_batch_size=2,
+    )
+    print(conf)
+    vars(conf)
+    if use_gpu:
+        from csbdeep.utils.tf import limit_gpu_memory
+
+        # adjust as necessary: limit GPU memory to be used by TensorFlow to leave some to OpenCL-based computations
+        # limit_gpu_memory(0.8)
+        # alternatively, try this:
+        limit_gpu_memory(None, allow_growth=True)
+
+    model = StarDist3D(conf, name="stardist", basedir="models")
+
+    median_size = calculate_extents(Y, np.median)
+    fov = np.array(model._axes_tile_overlap("ZYX"))
+    print(f"median object size:      {median_size}")
+    print(f"network field of view :  {fov}")
+    if any(median_size > fov):
+        print(
+            "WARNING: median object size larger than field of view of the neural network."
+        )
+
+    def random_fliprot(img, mask, axis=None):
+        if axis is None:
+            axis = tuple(range(mask.ndim))
+        axis = tuple(axis)
+
+        assert img.ndim >= mask.ndim
+        perm = tuple(np.random.permutation(axis))
+        transpose_axis = np.arange(mask.ndim)
+        for a, p in zip(axis, perm):
+            transpose_axis[a] = p
+        transpose_axis = tuple(transpose_axis)
+        img = img.transpose(transpose_axis + tuple(range(mask.ndim, img.ndim)))
+        mask = mask.transpose(transpose_axis)
+        for ax in axis:
+            if np.random.rand() > 0.5:
+                img = np.flip(img, axis=ax)
+                mask = np.flip(mask, axis=ax)
+        return img, mask
+
+    def random_intensity_change(img):
+        return img * np.random.uniform(0.6, 2) + np.random.uniform(-0.2, 0.2)
+
+    def augmenter(x, y):
+        """Augmentation of a single input/label image pair.
+        x is an input image
+        y is the corresponding ground-truth label image
+        """
+        # Note that we only use fliprots along axis=(1,2), i.e. the yx axis
+        # as 3D microscopy acquisitions are usually not axially symmetric
+        x, y = random_fliprot(x, y, axis=(1, 2))
+        x = random_intensity_change(x)
+        return x, y
+
+    model.train(
+        X_trn,
+        Y_trn,
+        validation_data=(X_val, Y_val),
+        augmenter=augmenter,
+        epochs=NUM_EPOCHS,
+    )
+
+    # Threshold optimization
+    # model.optimize_thresholds(X_val, Y_val)
+    model.optimize_thresholds(X_trn, Y_trn)
+
+    conf = None
+    model = None
+    # free up memory
+    tf.keras.backend.clear_session()
+    del conf
+    del model
+    from numba import cuda
+
+    cuda.select_device(0)
+    cuda.close()
+
+    # once done, results are saved to ./models/stardist. We want to rename the folder to include the seed and split
+    model_path = Path("models") / "stardist"
+    new_model_path = Path(f"models/stardist_{data}_{train_percentage}")
+    new_model_path.mkdir(parents=True, exist_ok=True)
+    try:
+        # copy the model to a new folder with the seed and split. Do not rename.
+        files = sorted(model_path.glob("*"))
+        # skip the "logs" folder as the permissions are problematic
+        files = [f for f in files if "logs" not in str(f)]
+        for file in files:
+            new_file = new_model_path / file.name
+            try:
+                shutil.copy2(file, new_file)
+            except PermissionError:
+                print(
+                    f"Could not copy {file} to {new_file}. Permission denied."
+                )
+                continue
+    except Exception as e:
+        print(f"Could not copy {model_path} to {new_model_path}.")
+        print("Training will be stopped to avoid overwriting the model.")
+        raise SystemError("Could not copy files") from e
+
+
+if __name__ == "__main__":
+    for split in TRAINING_PERCENTAGES:
+        for data in SPLITS:
+            print(f"Training {split}% split with data {data}")
+            print("_" * 50)
+            path_images = DATA_PATH / f"{data}"
+            save_path = pt.Path(f"./models/stardist_{data}_{split}")
+            if save_path.exists():
+                print(f"Model {save_path} already exists. Skipping.")
+                continue
+            train_stardist(path_images, split, seed=SEED)
+            print(f"Training {split}% split with data {data} done.")
